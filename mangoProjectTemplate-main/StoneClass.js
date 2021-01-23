c7eee74cce915e1b7ce1d3d2a40e9229a88c3fe2
Mahika9265 <72756971+Mahika9265@users.noreply.github.com>
2021-01-23 14:47:03 -0800

    Add files via upload

---
+++ mangoProjectTemplate-main/StoneClass.js
@@ -0,0 +1,18 @@
+var options={
+    isStatic:false,
+    restitution:0,
+    friction:1,
+    density:1.2
+}
+function Preload(){
+    stone = loadImage("image/stone.png");
+}
+
+fly(this.sling.null = null);
+    
+
+
+function Draw(){
+    Image(stone, 250,340,200,300)
+    stone.display();
+}
